4b28b667170fde2979fe9667bf2e2d05fb050e78
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-16 13:50:10 +0300

    customer reducer

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const cash = useSelector(state => state.cash);
+  const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers);
   console.log(cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
+
+  const getCash = (cash) => {
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
+
   return (
     <div className="App">
-      <div style={{display: 'flex'}}>
-        <button>ADD</button>
-        <button>GET</button>
+      <div style={{ fontSize: '3rem' }}>{cash}</div>
+      <div style={{ display: 'flex' }}>
+        <button onClick={() => addCash(Number(prompt()))}>ADD</button>
+        <button onClick={() => getCash(Number(prompt()))}>GET</button>
       </div>
+      {customers.length > 0 ?
+        <div>
+
+        </div>  
+        :
+        <div style={{fontSize: '2rem'}}>
+          No client!
+        </div>
+      }
     </div>
   );
 }
